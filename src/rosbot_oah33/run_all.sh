36cd4fba907b14974860805d63620acb46e6b281
heilmao <oheilmann5@gmail.com>
2021-12-29 03:06:58 +0000

    deleted extra shell script

--- src/rosbot_oah33/run_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gnome-terminal -- 'rosrun rosbot_bath run_rosbot.sh'
-gnome-terminal -- 'rosrun rosbot_oah33 run_teleop_twist.sh'
-gnome-terminal -- 'rosrun rosbot_oah33 run_master.sh'
