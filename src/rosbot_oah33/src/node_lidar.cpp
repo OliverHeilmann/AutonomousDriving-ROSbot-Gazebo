58ea59e940a3edf221d53bba1be45d9505ef76ab
heilmao <oheilmann5@gmail.com>
2022-01-04 14:17:00 +0000

    lidar code updates

--- src/rosbot_oah33/src/node_lidar.cpp
+++ src/rosbot_oah33/src/node_lidar.cpp
@@ -3,6 +3,8 @@
 #include <std_msgs/Float64.h>
 #include <sensor_msgs/LaserScan.h>
 #include<cmath>
+#include<iostream>
+#include <vector>
 
 ros::Publisher lidar_headings;
 
@@ -13,13 +15,77 @@ float lidar_array[720];
 /* callback for scan data */
 void callback_scan(const sensor_msgs::LaserScan &msg){
 
-    // 
+    // get number of samples in lidar sample vector
+    int sample_num = msg.ranges.size();
 
-    for ( n=0 ; n<lidar_array.size() ; n++ )
+    // calculate scan coverage area in radians
+    float angle_range = msg.angle_increment * sample_num;
+
+    // how many segments should the lidar be broken into
+    int parts = 4; // EVEN NUMBERS ONLY
+
+    // how many samples per part (last segment may have less than the rest)
+    int remainder = sample_num % parts;
+    int samples_per_part = (sample_num - remainder) / parts;
+
+    std::cout<< "Remainder: " << std::to_string(remainder) << std::endl;
+    std::cout<< "SPP: " << std::to_string(samples_per_part) << std::endl;
+    std::cout<< "Samples: " << std::to_string(sample_num) << std::endl;
+
+    // minimumm acceptable range of obstacles [m]
+    float min_obj_range = 1.;
+
+    // make 2D array of lidar headings 
+    float lidar_headings[parts] = {};
+
+    int step = 0;
+    bool moveable = true;
+    for (int i = 0; i <= sample_num-1; i++)
     {
-        result += lidar_array[n];
+        // if we are at a new segment, then store movable state and reset movable to default state
+        // (which is considered as true)
+        if ( i % samples_per_part == 0)
+        {
+            moveable = true;
+        }
+        // if at middle of part, store its heading in lidar_headings array
+        else if  (i % (samples_per_part/ 2) == 0)
+        {
+            if (lidar_headings[step] != -999.)
+            {
+                lidar_headings[step] = (i - samples_per_part/2 ) * msg.angle_increment;
+            }
+
+            std::cout << std::to_string(i) << ") Heading: " << std::to_string( lidar_headings[step] ) << std::endl;
+            //std::cout << std::to_string(i) << ") Step: " << std::to_string(step ) << std::endl;
+            
+            step++;
+        }
+
+        // if there is a range less than the minimum acceptable in this part, set movable to false
+        // i.e. robot cannot move here...
+        if (msg.ranges[i - samples_per_part/2] <= min_obj_range)
+        {
+            moveable = false;
+            lidar_headings[step] = -999.;
+        }
+
+        /*
+        else if ( i % (samples_per_part-1) == 0)
+        {
+            if (moveable == false)
+            {
+                lidar_headings[step] == -999.;
+            }
+
+            std::cout << std::to_string(lidar_headings[step]) << ", ";
+        }
+        */
     }
-    cout << result;
+
+    for (int i = 0; i <= parts-1; i++){
+        std::cout << std::to_string(lidar_headings[i]) << ", ";
+    }
 }
 
 /* setup callback as start, stop, reset, _, ... */
